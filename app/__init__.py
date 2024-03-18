5caa0ac17c63e9a3f9a953bda95a630d439a1f44
jabad88 <abadiesjosh@gmail.com>
2024-03-17 21:16:24 -0700

    lebron function now calls players from db

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,10 @@ from flask import Flask, flash, redirect,render_template,request,session,abort, 
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///players1.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://///Users/josh/Documents/flask_pyspot/app/instance/players1.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
 from app import routes
+
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
-from flask import flash, redirect,render_template,request,session,abort, render_template_string
+from flask import flash, redirect,render_template,request,session,abort, render_template_string, send_from_directory
 from app import app
 from app.models import Player
+import os
 
 @app.route("/")
 def index():
@@ -20,10 +21,20 @@ def display_images():
 def all_players_list():
     return "<h1>{{ list all players in db through this endpoint }}</h1>"
 
-@app.route("/lebron")
-def lebron():
-    return render_template("template.html",name=Player, quote=nba_players[0]['quote'], img=nba_players[0]['img'])
+@app.route("/players/<string:user_id>")
+def lebron(user_id):
+    lebron = Player.query.get(user_id)
+    
+    if lebron:
+        return render_template("template.html",name=(lebron.first + " "+  lebron.last), quote=lebron.quote)
+    else:
+        return "Something went wrong finding Lebron"
 
-@app.route("/kobe")
-def kobe():
-    return render_template("template.html",name=nba_players[1]['name'], quote=nba_players[1]['quote'], img=nba_players[1]['img'])
+# @app.route("/kobe")
+# def kobe():
+#     return render_template("template.html",name=nba_players[1]['name'], quote=nba_players[1]['quote'], img=nba_players[1]['img'])
+    
+
+#TODO Get pictures to work for indiviudals players
+#Fixed: the players in the database get called from def lebron. Reword this function
+#Consider making a frontend for this website.
